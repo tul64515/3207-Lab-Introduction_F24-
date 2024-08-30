d59a4385bc2b330af07ee1db8c8d5ec0c23c0c54
tul64515 <tul64515@temple.edu>
2024-08-30 14:44:26 -0400

    changes after new code from professor

--- random.c
+++ random.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "print_random.c"
 
-char rand_char()
+char randchar()
 {
     char c = 'a' + (random() % 26);
-}
-
-void rand_string(char *s, size_t size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        s[i] = rand_char();
-    }
-}
-
-int main()
-{
-    srand(time(NULL));
-    printf("%c\n", rand_char());
-
-    printf("%s\n", rand_string_alloc(7));
 }
